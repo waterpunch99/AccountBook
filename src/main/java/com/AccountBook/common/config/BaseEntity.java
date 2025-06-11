7c4078dda3a22914e577beb14a8abefa1e2d2fd5
Lee DongUK <130825615+waterpunch99@users.noreply.github.com>
2025-06-11 23:28:05 +0900

    Feat: BaseEntity 작성

---
+++ src/main/java/com/AccountBook/common/config/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.AccountBook.common.config;
+
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity {
+    @CreatedDate
+    private LocalDate CreateAt;
+    @LastModifiedDate
+    private LocalDate UpdateAt;
+}
